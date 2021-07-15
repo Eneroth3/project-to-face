0190b7bec30c5f51e769394d534442789ad817de
Eneroth3 <eneroth3@gmail.com>
2021-07-15 09:58:30 +0200

    Scaffold projection method

---
+++ project.rb
@@ -0,0 +1,16 @@
+module Project
+  def self.project(source_instances, face_path)
+    p source_instances
+    p face_path
+    p face_path.to_a
+    puts
+
+    # Iterate instances
+    # Create instances inside of face parent (inside its own group)
+    # Tag group
+    # Calculate flat transformations on face's plane
+    # Explode
+    # Reset tags?
+    # Crop
+  end
+end
--- project_tool.rb
+++ project_tool.rb
@@ -1,14 +1,14 @@
-reqire_relative "project"
+require_relative "project"
 
 class ProjectTool
   def activate
-    @target_face_path = nil
+    @hovered_face_path = nil
     @source_instances = []
-    
+
     # TODO: If usable_selection?
     @source_instances = Sketchup.active_model.selection.to_a
   end
-  
+
   def onMouseMove(flags, x, y, view)
     ph = view.pick_helper(x, y)
     if @source_instances.empty? # TODO: or Ctrl is pressed
@@ -17,11 +17,11 @@ class ProjectTool
       hover_face(ph)
     end
   end
-  
+
   def hover_source(ph)
     model = Sketchup.active_model
     model.selection.clear
-    
+
     model.selection.add(@source_instances)
     instance = ph.best_picked
     if instance && instance?(instance)
@@ -33,17 +33,13 @@ class ProjectTool
     model = Sketchup.active_model
     model.selection.clear
     model.selection.add(@source_instances)
-    
-    face = ph.picked_face
-    return unless face
-    
-    ### transformation = transformation_from_ph(ph, face)
-    # TODO: Save path? Or save transformation?
-    # hovered_face_path
-    
-    model.selection.add(face)
+
+    @hovered_face_path = grep_path(ph, Sketchup::Face)
+    return unless @hovered_face_path
+
+    model.selection.add(@hovered_face_path.leaf)
   end
-  
+
   def onLButtonDown(flags, x, y, view)
     if @source_instances.empty? # TODO: or Ctrl is pressed
       click_instance
@@ -52,27 +48,37 @@ class ProjectTool
     end
     # TODO: Update status text. Also on activate.
   end
-  
+
   def click_instance
     model = Sketchup.active_model
     @source_instances = model.selection.select { |e| instance?(e) }
   end
-  
+
   def click_face
-    puts "Project"
-    # take face from selection
-    # project stuff to it.
+    Project.project(@source_instances, @hovered_face_path)
     reset
   end
-  
+
   def reset
-    @target_face_path = nil
+    @hovered_face_path = nil
     @source_instances = []
     Sketchup.active_model.selection.clear
   end
-  
+
   def instance?(entity)
     [Sketchup::Group, Sketchup::ComponentInstance].include?(entity.class)
+  end
+
+  # @return [Sketchup::InstancePath, nil]
+  def grep_path(ph, klass)
+    ph.count.times do |i|
+      path = ph.path_at(i)
+      next unless path.last.is_a?(klass)
+
+      return Sketchup::InstancePath.new(path)
+    end
+
+    nil
   end
 end
 
