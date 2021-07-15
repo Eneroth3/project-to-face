9f981a49b35855bfedbb4227066d3c5dd508c680
Eneroth3 <eneroth3@gmail.com>
2021-07-15 11:53:09 +0200

    Documenting and cleaning

--- project.rb
+++ project.rb
@@ -1,14 +1,31 @@
 module Project
-  def self.project(source_instances, face_path)
+  # Project groups/components onto a faces nested in a group/component.
+  #
+  # @param source_instances [Array<Sketchup::group, Sketchup::Component>]
+  #   Assumed to be ion the active entities.
+  # @param face_path [Sketchup::InstancePath]
+  # @param explode [Boolean]
+  #   Explode projection to geometry.
+  # @param crop [Boolean]
+  #   Remove edges outside of face's boundary.
+  #   (requires `explode` to be true)
+  # @param purge_faces [Boolean]
+  #   Remove faces, projects wire frame.
+  #   (requires `explode` to be true)
+  # @param purge_layers [Boolean]
+  #   make projection untagged.
+  #   (requires `explode` to be true)
+  # @param
+  def self.project(source_instances, face_path, explode = true, crop = true, purge_faces = true, purge_layers = true)
     face = face_path.leaf
     face_parent = face_path.to_a[-2].definition
-    projection_group = face_parent.entities.add_group # has identity transformation.
+    projection_group = face_parent.entities.add_group # Has identity transformation.
 
     # Copy instances to corresponding place inside face's container.
     new_instances = source_instances.map do |source_instance|
       projection_group.entities.add_instance(
         source_instance.definition,
-        face_path.transformation.inverse * source_instance.transformation # REVIEW: Check order.
+        face_path.transformation.inverse * source_instance.transformation
       )
     end
 
@@ -23,37 +40,44 @@ module Project
     )
     new_instances.each { |i| i.transform!(flatten_tr) }
 
-    # Explode
-    # TODO: Add user option for this.
-    while
-      instances = projection_group.entities.select { |e| instance?(e) }
-      break if instances.empty?
+    if explode
+      while
+        instances = projection_group.entities.select { |e| instance?(e) }
+        break if instances.empty?
 
-      instances.each(&:explode)
+        instances.each(&:explode)
+      end
+
+      projection_group.entities.each { |e| e.layer = nil } if purge_layers
+
+      if crop
+        boundary_points = face.vertices.map(&:position)
+        # HACK: explode a temp group to merge edges.
+        temp_group = projection_group.entities.add_group
+        temp_face = temp_group.entities.add_face(boundary_points)
+        temp_face.erase!
+        temp_group.explode
+        projection_group.entities.to_a.each do |edge|
+          next unless edge.is_a?(Sketchup::Edge)
+          next if on_face?(face, midpoint(edge))
+
+          edge.erase!
+        end
+      end
+
+      # Purge faces (want a wire frame)
+      if purge_faces
+        projection_group.entities.erase_entities(projection_group.entities.grep(Sketchup::Face))
+      end
     end
-
-    # TODO: Add user option
-    projection_group.entities.each { |e| e.layer = nil }
-
-    # Crop
-    boundary_points = face.vertices.map(&:position)
-    # HACK: explode a temp group to merge edges.
-    temp_group = projection_group.entities.add_group
-    temp_face = temp_group.entities.add_face(boundary_points)
-    temp_face.erase!
-    temp_group.explode
-    projection_group.entities.to_a.each do |edge|
-      next unless edge.is_a?(Sketchup::Edge)
-      next if on_face?(face, midpoint(edge))
-
-      edge.erase!
-    end
-
-    # Purge faces (want a wire frame)
-    projection_group.entities.erase_entities(projection_group.entities.grep(Sketchup::Face))
   end
 
-  # "transform" the base transformation by a modifier transformation.
+  # "Transform" the base transformation by a modifier transformation.
+  #
+  # @param base [Geom::Transformation]
+  # @param modifier [Geom::Transformation]
+  #
+  # @return [Geom::Transformation]
   def self.transform_transformation(base, modifier)
     modifier*base*modifier.inverse
   end
@@ -63,10 +87,21 @@ module Project
     [Sketchup::Group, Sketchup::ComponentInstance].include?(entity.class)
   end
 
+  # Find midpoint for edge.
+  #
+  # @param edge [Sketchup::Edge]
+  #
+  # @return [Geom::Point3d]
   def self.midpoint(edge)
     Geom.linear_combination(0.5, edge.start.position, 0.5, edge.end.position)
   end
 
+  # Test if a point is on a face.
+  #
+  # @param [Sketchup::Face]
+  # @param [Geom::Point3d]
+  #
+  # @return [Boolean]
   def self.on_face?(face, point)
     face.classify_point(point) == Sketchup::Face::PointInside
   end
